247542cc4571b666a09c87ed7b8a55104bc8f4d8
sivanesh1996 <vssivanesh@gmail.com>
2022-10-11 16:27:35 +0530

    printing obj

---
+++ Trick1.java
@@ -0,0 +1,13 @@
+package tricks;
+
+public class Trick1 {
+
+	public static void main(String[] args) {
+		Trick1 s=new Trick1();
+		Trick1 s1=new Trick1();
+		System.out.println(s);
+		System.out.println(s1);
+
+	}
+
+}
